786ca5774c42b516ac80e57b878d880448375107
rafaelsolislopez <rafasolis1984@gmail.com>
2025-03-27 13:28:37 +0100

    add get method

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from mockusers import MockUsers
 
 #url local: http:127.0.0.1:8000
-app=FastAPI()
+"""
+URL local: http:127.0.0.1:800
+levantar el servidor: python -m uvicorn main:app --reload (Ctr + c para cerrarlo)
 
-@app.get("/")
-async def root():
-    return "Hello World"  
-#python -m uviccorn api/main:app --reload
+CRUD (Create, Read, Update , Delete)
+POST: inserción de datos
+GET:lectura de datos
+PUT:actualización de datos
+DEELTE: borrado de datos
+"""
+app=FastAPI()
+#Mock sample list users
+user_list=MockUsers().get_user_list()
+
+
+@app.get("/users")
+async def users():
+    return user_list  
+
+
+@app.get("/user/{id}")
+async def user(id:int):
+    user=filter(lambda user:user.id==id,user_list )
+    return user
+
+
+
+
---
+++ backend/mockusers.py
@@ -0,0 +1,11 @@
+from user import User
+
+class MockUsers():
+    def __init__(self):
+        self.user1=User(id=1,name="Rafa",mail="rafa@mail.com",age=41)
+        self.user2=User(id=2,name="Leo",mail="leo@mail.com",age=9)
+        self.user3=User(id=3.name="jorge",mail="gorge@mail.com",age=6)
+
+    def get_user_list(self):
+        user_list=[self.user1,self.user2,self.user3]
+        return user_list
