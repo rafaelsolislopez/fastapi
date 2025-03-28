4a95951ecc912fd673087fce3f561af6b75cf45c
rafaelsolislopez <rafasolis1984@gmail.com>
2025-03-28 19:16:20 +0100

    del backend

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from fastapi import APIRouter,HTTPException
 import logging
-from mockusers import MockUsers
+
 
 class User(BaseModel):
     id:int
@@ -10,9 +10,22 @@ class User(BaseModel):
     age:int
 
 
+class MockUsers():
+    def __init__(self):
+        self.user1=User(id=1,name="Rafa",mail="rafa@mail.com",age=41)
+        self.user2=User(id=2,name="Leo",mail="leo@mail.com",age=9)
+        self.user3=User(id=3,name="jorge",mail="gorge@mail.com",age=6)
 
-#URL local: http:127.0.0.1:800
-#levantar el servidor: python -m uvicorn main:app --reload (Ctr + c para cerrarlo)
+    def get_user_list(self)->list:
+        user_list=[self.user1,self.user2,self.user3]
+        return user_list
+    
+    def new_user(self,id:int)->User:
+        self.user=User(id=id,name=f"Name{id}",mail="name{id}@mail.com",age=id*5)
+        return self.user
+
+
+
 
 """
 CRUD (Create, Read, Update , Delete)
@@ -41,7 +54,7 @@ def searh_user(id:int):
         return {"detail":"error searching user"}
 
 
-@router.get("/users",request_model=User,status_code=201 )
+@router.get("/users",response_model=User,status_code=201 )
 async def users():
     return user_list  
 
